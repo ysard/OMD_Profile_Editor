c6d0cbd1365faaeb6b62ccf2404e781277754e33
ysard <ysard@users.noreply.github.com>
2024-02-22 03:11:40 +0100

    Add licence

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# OMD_profile_editor a savegame editor for Orcs Must Die! Tower Defense game.
+# Copyright (C) 2024  Ysard
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 <Profiles lastlogin="%s" version="%d">
 
