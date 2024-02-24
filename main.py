47bfaa872f3b7bfefaa7faee0edcd58fb0915c92
ysard <ysard@users.noreply.github.com>
2024-02-24 22:15:30 +0100

    Purge legacycode, use original method obtained by reversing to crypt the file

--- main.py
+++ main.py
@@ -80,90 +80,6 @@ import xml.etree.ElementTree as ET
 import argparse
 
 
-MAPPING = {
-    0x4B: "a",
-    0x48: "b",
-    0x49: "c",
-    0x4E: "d",
-    0x4F: "e",
-    0x4C: "f",
-    0x4D: "g",
-    0x42: "h",
-    0x43: "i",
-    0x40: "j",
-    0x41: "k",
-    0x46: "l",
-    0x47: "m",
-    0x44: "n",
-    0x45: "o",
-    0x5A: "p",
-    0x5B: "q",
-    0x58: "r",
-    0x59: "s",
-    0x5E: "t",
-    0x5F: "u",
-    0x5C: "v",
-    0x5D: "w",
-    0x52: "x",
-    0x53: "y",
-    0x50: "z",
-    0x6B: "A",
-    0x68: "B",
-    0x69: "C",
-    0x6E: "D",
-    0x6F: "E",
-    0x6C: "F",
-    0x6D: "G",
-    0x62: "H",
-    0x63: "I",
-    0x60: "J",
-    0x61: "K",
-    0x66: "L",
-    0x67: "M",
-    0x64: "N",
-    0x65: "O",
-    0x7A: "P",
-    0x7B: "Q",
-    0x78: "R",
-    0x79: "S",
-    0x7E: "T",
-    0x7F: "U",
-    0x7C: "V",
-    0x7D: "W",
-    0x72: "X",
-    0x73: "Y",
-    0x70: "Z",
-    # '0x?!': '!',
-    # '0x?&': '&',
-    0x08: '"',
-    0x05: "/",
-    0x16: "<",
-    0x17: "=",
-    0x14: ">",
-    0x1A: "0",
-    0x1B: "1",
-    0x18: "2",
-    0x19: "3",
-    0x1E: "4",
-    0x1F: "5",
-    0x1C: "6",
-    0x1D: "7",
-    0x12: "8",
-    0x13: "9",
-    0x0A: " ",
-    0x23: "\t",
-    0x20: "\n",
-    # "0x?'": "'",
-    # '0x?(': '(',
-    # '0x?)': ')',
-    # '0x?*': '*',
-    # '0x?+': '+',
-    0x75: "-",  # ??????
-    0x04: ".",
-    # '0x??': '?',
-    0x56: "_",
-}
-
 SKULLUPGRADES = {
     "1": "mur de flèches étendu",
     "3": "barricade améliorée",
@@ -195,10 +111,8 @@ def decrypt(profile, *args, skull_upgrades=None, **kwargs):
     profile_path = Path(profile)
     data = profile_path.read_bytes()
 
-    text = ""
-    for byte in data:
-        # text += MAPPING.get(byte, " ") #f"x{hex(byte)}")
-        text += MAPPING.get(byte, f"|{hex(byte)}|")
+    # Use the key to XOR each bytes
+    text = "".join(map(chr, [byte ^ 42 for byte in data]))
 
     profile_clear_path = Path(profile_path.parent / "profiles_clear.xml")
     profile_clear_path.write_text(text, encoding="utf8")
@@ -212,13 +126,8 @@ def crypt(profile, *args, **kwargs):
     profile_clear_path = Path(profile)
     text = profile_clear_path.read_text(encoding="utf8")
 
-    rev_mapping = {v: k for k, v in MAPPING.items()}
-
-    try:
-        data = bytes(rev_mapping[char] for char in text)
-    except KeyError as e:
-        print("Error: Character not known in the charset:", e)
-        raise SystemExit(42) from e
+    # Use the key to XOR each char
+    data = bytes([ord(char) ^ 42 for char in text])
 
     # Make a backup of the previous profile
     crypt_profile = Path(profile_clear_path.parent / "profiles.xml")
